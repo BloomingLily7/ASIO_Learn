97c9ca43a9a3dcebd7350badf6f62fa75ef3ca64
Jude <761018528@qq.com>
2025-07-20 00:37:01 +0800

    同步读写客户端&服务器

---
+++ SyncClient/SyncClient/SyncClient.cpp
@@ -0,0 +1,51 @@
+﻿/*
+同步通信客户端
+1.根据对端ip和端口创建endpoint
+2.创建socket连接endpoint
+3.采用同步读写方式通信
+*/
+#include <iostream>
+#include<boost/asio.hpp>
+using namespace std;
+using namespace boost::asio::ip;
+const int  MAX_LENGTH = 1024;//客户端允许发送的最大字节长度
+
+int main()
+{
+    try {
+        //创建上下文环境
+        boost::asio::io_context ioc;
+        //构建endpoint
+        tcp::endpoint remote_ep(make_address("127.0.0.1"), 10086);
+
+        tcp::socket skt(ioc);
+        boost::system::error_code error = boost::asio::error::host_not_found;
+        skt.connect(remote_ep, error);
+
+        if (error) {
+            cout << "connect failed. Code is: " << error.value();
+            cout << "error message is: " << error.message();
+            return 0;
+        }
+
+        cout << "Enter Message:";
+        char request[MAX_LENGTH];
+        cin.getline(request, MAX_LENGTH);
+        size_t request_length = strlen(request);
+        boost::asio::write(skt, boost::asio::buffer(request, request_length));
+
+        char reply[MAX_LENGTH];
+        size_t reply_length = boost::asio::read(skt, boost::asio::buffer(reply, request_length));
+        cout << "Reply is: ";
+        cout.write(reply, reply_length);
+        cout << "\n";
+
+    }
+    catch (exception& e)
+    {
+        cerr << "Exception: " << e.what() << endl;
+    }
+    return 0;
+}
+
+
---
+++ SyncServer/SyncServer/SyncServer.cpp
@@ -0,0 +1,80 @@
+﻿/*
+* 同步通信服务端
+*   服务器启动，监听指定端口
+    当新客户端连接时：
+    创建新socket
+    创建新线程处理该连接
+    线程中循环读取客户端数据并回传
+    客户端断开连接时，线程结束
+    服务器持续运行直到被手动终止
+*/
+#include <iostream>
+#include <boost/asio.hpp>
+#include <set>
+using  boost::asio::ip::tcp;
+using namespace std;
+const int MAX_LENGTH = 1024;
+
+//使用shared_ptr管理TCP socket对象
+typedef std::shared_ptr<tcp::socket> skt_ptr;
+
+//使用set集合管理所有客户端线程
+std::set<std::shared_ptr<std::thread>> thread_set;
+
+void session(skt_ptr skt)
+{
+    try {
+        for (;;) {
+            char data[MAX_LENGTH];
+            memset(data, '\0', MAX_LENGTH);
+            boost::system::error_code error;
+            size_t length = skt->read_some(boost::asio::buffer(data, MAX_LENGTH), error);
+
+            if (error == boost::asio::error::eof) {
+                cout << "connect is closed by peer" << endl;
+                break;
+            }
+            else if (error) {
+                throw boost::system::system_error(error);
+            }
+            cout << "receive from" << skt->remote_endpoint().address().to_string() << endl;
+            cout << "recevice message is" << data << endl;
+
+            boost::asio::write(*skt, boost::asio::buffer(data, length));
+        }
+    }
+    catch (exception& e)
+    {
+        cerr << "Exception in thread: " << e.what() << endl;
+    }
+}
+
+void server(boost::asio::io_context& ioc,unsigned short port) {
+    tcp::acceptor a(ioc, tcp::endpoint(tcp::v4(), port));
+    for (;;)
+    {
+        skt_ptr skt(new tcp::socket(ioc));
+        a.accept(*skt);
+        auto t = std::make_shared<std::thread>(session, skt);
+        thread_set.insert(t);
+    }
+}
+
+
+int main()
+{
+    try{
+        boost::asio::io_context ioc;
+        server(ioc, 10086);
+        for (auto& t : thread_set)
+        {
+            t->join();
+        }
+    }
+    catch (exception& e)
+    {
+        cerr << "Exception is" << e.what() << endl;
+    }
+    return 0;
+}
+
