8f5613573b8faf831959745f2d2437fc09480470
Jude <761018528@qq.com>
2025-07-21 19:34:39 +0800

    完善了同步读写服务端的注释

--- SyncServer/SyncServer/SyncServer.cpp
+++ SyncServer/SyncServer/SyncServer.cpp
@@ -21,11 +21,13 @@ typedef std::shared_ptr<tcp::socket> skt_ptr;
 //使用set集合管理所有客户端线程
 std::set<std::shared_ptr<std::thread>> thread_set;
 
+//读取客户端发来的数据并回显
 void session(skt_ptr skt)
 {
     try {
         for (;;) {
             char data[MAX_LENGTH];
+            //先将缓冲区清空
             memset(data, '\0', MAX_LENGTH);
             boost::system::error_code error;
             size_t length = skt->read_some(boost::asio::buffer(data, MAX_LENGTH), error);
@@ -39,7 +41,7 @@ void session(skt_ptr skt)
             }
             cout << "receive from" << skt->remote_endpoint().address().to_string() << endl;
             cout << "recevice message is" << data << endl;
-
+            //回显
             boost::asio::write(*skt, boost::asio::buffer(data, length));
         }
     }
@@ -49,12 +51,17 @@ void session(skt_ptr skt)
     }
 }
 
+//接收新的连接并分配线程
 void server(boost::asio::io_context& ioc,unsigned short port) {
+    //创建TCP acceptor监听指定端口
     tcp::acceptor a(ioc, tcp::endpoint(tcp::v4(), port));
+    //进入无限循环接受新连接：
     for (;;)
     {
+        //创建新的socket对象
         skt_ptr skt(new tcp::socket(ioc));
         a.accept(*skt);
+        //创建新线程处理该连接
         auto t = std::make_shared<std::thread>(session, skt);
         thread_set.insert(t);
     }
@@ -63,6 +70,15 @@ void server(boost::asio::io_context& ioc,unsigned short port) {
 
 int main()
 {
+    /*
+    创建I/O上下文对象io_context
+
+    调用server()函数启动服务器，监听10086端口
+
+    等待所有客户端线程结束（join）
+
+    捕获并处理可能发生的异常
+*/
     try{
         boost::asio::io_context ioc;
         server(ioc, 10086);
